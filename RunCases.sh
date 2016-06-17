99072c12cb9c7405070866629442bf72b6421df5
qiujieqiong@linuxdeepin.com <qiujieqiong@linuxdeepin.com>
2016-06-17 16:55:57 +0800

    Copy from lava-test

--- RunCases.sh
+++ RunCases.sh
@@ -19,7 +19,7 @@ systemctl is-active lightdm >/dev/null && systemctl stop lightdm || true
 
 
 systemctl start lightdm
-sleep 50
+sleep 30
 # wait for the launching of desktop till timeout
 ps aux |grep dde-dock |grep -v grep
 if [[ $? == 0 ]]; then
